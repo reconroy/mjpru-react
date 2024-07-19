d648039cd1d8e9df90e89e6035640891ddbcd249
jayantroy <141987105+reconroy@users.noreply.github.com>
2024-07-19 11:14:19 +0530

    Added Dynamic Entry in Academic And Research Qualification with CRUD Features

--- src/components/user/RegistrationData/AcademicQualifications.jsx
+++ src/components/user/RegistrationData/AcademicQualifications.jsx
@@ -1,11 +1,66 @@
 import React, { useState } from 'react';
 import { HiAcademicCap } from "react-icons/hi2";
 import { BiSolidEdit } from "react-icons/bi";
+import { TiDelete } from "react-icons/ti";
 import { Modal, Button } from 'react-bootstrap';
 import "./../../../customStyles/buttonAnimation.css";
 
 const AcademicQualifications = () => {
   const [selectedRow, setSelectedRow] = useState(null);
+  const [showAddModal, setShowAddModal] = useState(false);
+  const [qualifications, setQualifications] = useState([
+    {
+      name: 'High School (10th)',
+      year: '',
+      board: '',
+      subjects: '',
+      marksObtained: '',
+      marksType: '',
+      percentage: '',
+      division: ''
+    },
+    {
+      name: 'Intermediate (12th)',
+      year: '',
+      board: '',
+      subjects: '',
+      marksObtained: '',
+      marksType: '',
+      percentage: '',
+      division: ''
+    },
+    {
+      name: 'Undergraduate',
+      year: '',
+      board: '',
+      subjects: '',
+      marksObtained: '',
+      marksType: '',
+      percentage: '',
+      division: ''
+    },
+    {
+      name: 'Post Undergraduate',
+      year: '',
+      board: '',
+      subjects: '',
+      marksObtained: '',
+      marksType: '',
+      percentage: '',
+      division: ''
+    }
+  ]);
+
+  const [newQualification, setNewQualification] = useState({
+    name: '',
+    year: '',
+    board: '',
+    subjects: '',
+    marksObtained: '',
+    marksType: '',
+    percentage: '',
+    division: ''
+  });
 
   const handleShowModal = (index) => {
     setSelectedRow(index);
@@ -15,12 +70,55 @@ const AcademicQualifications = () => {
     setSelectedRow(null);
   };
 
-  const qualifications = [
-    'High School (10th)',
-    'Intermediate (12th)',
-    'Undergraduate',
-    'Post Undergraduate'
-  ];
+  const handleShowAddModal = () => {
+    setShowAddModal(true);
+  };
+
+  const handleCloseAddModal = () => {
+    setShowAddModal(false);
+  };
+
+  const handleNewQualificationChange = (e) => {
+    const { name, value } = e.target;
+    setNewQualification(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  const handleQualificationChange = (e) => {
+    const { name, value } = e.target;
+    const updatedQualifications = [...qualifications];
+    updatedQualifications[selectedRow] = {
+      ...updatedQualifications[selectedRow],
+      [name]: value
+    };
+    setQualifications(updatedQualifications);
+  };
+
+  const handleAddQualification = () => {
+    setQualifications([...qualifications, newQualification]);
+    setNewQualification({
+      name: '',
+      year: '',
+      board: '',
+      subjects: '',
+      marksObtained: '',
+      marksType: '',
+      percentage: '',
+      division: ''
+    });
+    handleCloseAddModal();
+  };
+
+  const handleDeleteQualification = (index) => {
+    const updatedQualifications = qualifications.filter((_, i) => i !== index);
+    setQualifications(updatedQualifications);
+  };
+
+  const handleSaveChanges = () => {
+    handleCloseModal();
+  };
 
   return (
     <div className="card">
@@ -29,7 +127,7 @@ const AcademicQualifications = () => {
           <HiAcademicCap className="me-2" />Academic Qualification
         </div>
         <div className="right">
-          <button type='button' className="btn btn-light card-button">Add Other Qualification</button>
+          <button type='button' className="btn btn-light card-button" onClick={handleShowAddModal}>Add Other Qualification</button>
         </div>
       </div>
       <div className="container mt-3">
@@ -53,18 +151,25 @@ const AcademicQualifications = () => {
               {qualifications.map((exam, index) => (
                 <tr key={index}>
                   <th scope="row" style={{ textAlign: "center" }}>{index + 1}</th>
-                  <td>{exam}</td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
+                  <td>{exam.name}</td>
+                  <td>{exam.year}</td>
+                  <td>{exam.board}</td>
+                  <td>{exam.subjects}</td>
+                  <td>{exam.marksObtained}</td>
+                  <td>{exam.marksType}</td>
+                  <td>{exam.percentage}</td>
+                  <td>{exam.division}</td>
                   <td>
-                    <button type="button" className="btn btn-link p-0" onClick={() => handleShowModal(index)}>
-                      <BiSolidEdit size="25" />
-                    </button>
+                    <div className="d-flex justify-content-start">
+                      <button type="button" className="btn btn-link p-0" onClick={() => handleShowModal(index)}>
+                        <BiSolidEdit size="25" />
+                      </button>
+                      {index >= 4 && (
+                        <button type="button" className="btn btn-link p-0" onClick={() => handleDeleteQualification(index)}>
+                          <TiDelete size="25" color="red" />
+                        </button>
+                      )}
+                    </div>
                   </td>
                 </tr>
               ))}
@@ -84,42 +189,38 @@ const AcademicQualifications = () => {
             <form>
               <div className="mb-3">
                 <label className="form-label">Name of Examination</label>
-                <input type="text" className="form-control" value={qualifications[selectedRow]} readOnly />
+                <input type="text" className="form-control" value={qualifications[selectedRow].name} readOnly />
               </div>
               <div className="mb-3">
                 <label className="form-label">Year</label>
-                <input type="text" className="form-control" />
+                <input type="text" className="form-control" name="year" value={qualifications[selectedRow].year} onChange={handleQualificationChange} />
               </div>
               <div className="mb-3">
                 <label className="form-label">Board / University</label>
-                <input type="text" className="form-control" />
+                <input type="text" className="form-control" name="board" value={qualifications[selectedRow].board} onChange={handleQualificationChange} />
               </div>
               <div className="mb-3">
                 <label className="form-label">Subjects</label>
-                <input type="text" className="form-control" />
+                <input type="text" className="form-control" name="subjects" value={qualifications[selectedRow].subjects} onChange={handleQualificationChange} />
               </div>
               <div className="mb-3">
                 <label className="form-label">Marks Obtained</label>
-                <input type="text" className="form-control" />
-              </div>
-              <div className="mb-3">
-                <label className="form-label">Minimum Marks</label>
-                <input type="text" className="form-control" />
+                <input type="text" className="form-control" name="marksObtained" value={qualifications[selectedRow].marksObtained} onChange={handleQualificationChange} />
               </div>
               <div className="mb-3">
                 <label className="form-label">Marks Type</label>
-                <select className="form-select">
+                <select className="form-select" name="marksType" value={qualifications[selectedRow].marksType} onChange={handleQualificationChange}>
                   <option value="percentage">Percentage</option>
                   <option value="cgpa">CGPA</option>
                 </select>
               </div>
               <div className="mb-3">
                 <label className="form-label">Percentage / CGPA</label>
-                <input type="text" className="form-control" />
+                <input type="text" className="form-control" name="percentage" value={qualifications[selectedRow].percentage} onChange={handleQualificationChange} />
               </div>
               <div className="mb-3">
                 <label className="form-label">Division</label>
-                <select className="form-select">
+                <select className="form-select" name="division" value={qualifications[selectedRow].division} onChange={handleQualificationChange}>
                   <option value="first">First</option>
                   <option value="second">Second</option>
                   <option value="third">Third</option>
@@ -129,10 +230,64 @@ const AcademicQualifications = () => {
           </Modal.Body>
           <Modal.Footer>
             <Button variant="secondary" onClick={handleCloseModal}>Close</Button>
-            <Button variant="primary">Save changes</Button>
+            <Button variant="primary" onClick={handleSaveChanges}>Save changes</Button>
           </Modal.Footer>
         </Modal>
       )}
+      <Modal show={showAddModal} onHide={handleCloseAddModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add Qualification</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form>
+            <div className="mb-3">
+              <label className="form-label">Name of Examination</label>
+              <input type="text" className="form-control" name="name" value={newQualification.name} onChange={handleNewQualificationChange} />
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Year</label>
+              <input type="text" className="form-control" name="year" value={newQualification.year} onChange={handleNewQualificationChange} />
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Board / University</label>
+              <input type="text" className="form-control" name="board" value={newQualification.board} onChange={handleNewQualificationChange} />
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Subjects</label>
+              <input type="text" className="form-control" name="subjects" value={newQualification.subjects} onChange={handleNewQualificationChange} />
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Marks Obtained</label>
+              <input type="text" className="form-control" name="marksObtained" value={newQualification.marksObtained} onChange={handleNewQualificationChange} />
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Marks Type</label>
+              <select className="form-select" name="marksType" value={newQualification.marksType} onChange={handleNewQualificationChange}>
+                <option value="">Select</option>
+                <option value="percentage">Percentage</option>
+                <option value="cgpa">CGPA</option>
+              </select>
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Percentage / CGPA</label>
+              <input type="text" className="form-control" name="percentage" value={newQualification.percentage} onChange={handleNewQualificationChange} />
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Division</label>
+              <select className="form-select" name="division" value={newQualification.division} onChange={handleNewQualificationChange}>
+                <option value="">Select</option>
+                <option value="first">First</option>
+                <option value="second">Second</option>
+                <option value="third">Third</option>
+              </select>
+            </div>
+          </form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseAddModal}>Close</Button>
+          <Button variant="primary" onClick={handleAddQualification}>Add Qualification</Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
--- src/components/user/RegistrationData/ResearchQualifications.jsx
+++ src/components/user/RegistrationData/ResearchQualifications.jsx
@@ -1,11 +1,63 @@
 import React, { useState } from 'react';
 import { HiAcademicCap } from "react-icons/hi2";
 import { BiSolidEdit } from "react-icons/bi";
+import { TiDelete } from "react-icons/ti";
 import { Modal, Button } from 'react-bootstrap';
 import "./../../../customStyles/buttonAnimation.css";
 
 const ResearchQualifications = () => {
   const [selectedRow, setSelectedRow] = useState(null);
+  const [showAddModal, setShowAddModal] = useState(false);
+  const [newQualification, setNewQualification] = useState({
+    degree: '',
+    university: '',
+    subjects: '',
+    year: '',
+    marksType: '',
+    percentage: '',
+    status: ''
+  });
+
+  const initialQualifications = [
+    {
+      degree: 'M. Phil.',
+      university: '',
+      subjects: '',
+      year: '',
+      marksType: '',
+      percentage: '',
+      status: ''
+    },
+    {
+      degree: 'Ph.D / D.Phil.',
+      university: '',
+      subjects: '',
+      year: '',
+      marksType: '',
+      percentage: '',
+      status: ''
+    },
+    {
+      degree: 'D.Sc.',
+      university: '',
+      subjects: '',
+      year: '',
+      marksType: '',
+      percentage: '',
+      status: ''
+    },
+    {
+      degree: 'D.Litt.',
+      university: '',
+      subjects: '',
+      year: '',
+      marksType: '',
+      percentage: '',
+      status: ''
+    }
+  ];
+
+  const [qualifications, setQualifications] = useState(initialQualifications);
 
   const handleShowModal = (index) => {
     setSelectedRow(index);
@@ -15,12 +67,54 @@ const ResearchQualifications = () => {
     setSelectedRow(null);
   };
 
-  const qualifications = [
-    'M. Phil.',
-    'Ph.D / D.Phil.',
-    'D.Sc.',
-    'D.Litt.'
-  ];
+  const handleShowAddModal = () => {
+    setShowAddModal(true);
+  };
+
+  const handleCloseAddModal = () => {
+    setShowAddModal(false);
+  };
+
+  const handleNewQualificationChange = (e) => {
+    const { name, value } = e.target;
+    setNewQualification(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  const handleQualificationChange = (e) => {
+    const { name, value } = e.target;
+    const updatedQualifications = [...qualifications];
+    updatedQualifications[selectedRow] = {
+      ...updatedQualifications[selectedRow],
+      [name]: value
+    };
+    setQualifications(updatedQualifications);
+  };
+
+  const handleAddQualification = () => {
+    setQualifications([...qualifications, newQualification]);
+    setNewQualification({
+      degree: '',
+      university: '',
+      subjects: '',
+      year: '',
+      marksType: '',
+      percentage: '',
+      status: ''
+    });
+    handleCloseAddModal();
+  };
+
+  const handleDeleteQualification = (index) => {
+    const updatedQualifications = qualifications.filter((_, i) => i !== index);
+    setQualifications(updatedQualifications);
+  };
+
+  const handleSaveChanges = () => {
+    handleCloseModal();
+  };
 
   return (
     <div className="card">
@@ -29,7 +123,7 @@ const ResearchQualifications = () => {
           <HiAcademicCap className="me-2" />Research Qualification
         </div>
         <div className="right">
-          <button type='button' className="btn btn-light card-button">Add Other Qualification</button>
+          <button type='button' className="btn btn-light card-button" onClick={handleShowAddModal}>Add Other Qualification</button>
         </div>
       </div>
       <div className="container mt-3">
@@ -38,7 +132,7 @@ const ResearchQualifications = () => {
             <thead className="table-dark">
               <tr>
                 <th scope="col" style={{ textAlign: "center" }}>Sr. No.</th>
-                <th scope="col">Dgree</th>
+                <th scope="col">Degree</th>
                 <th scope="col">University</th>
                 <th scope="col">Subjects / Title</th>
                 <th scope="col">Year</th>
@@ -52,17 +146,24 @@ const ResearchQualifications = () => {
               {qualifications.map((exam, index) => (
                 <tr key={index}>
                   <th scope="row" style={{ textAlign: "center" }}>{index + 1}</th>
-                  <td>{exam}</td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
+                  <td>{exam.degree}</td>
+                  <td>{exam.university}</td>
+                  <td>{exam.subjects}</td>
+                  <td>{exam.year}</td>
+                  <td>{exam.marksType}</td>
+                  <td>{exam.percentage}</td>
+                  <td>{exam.status}</td>
                   <td>
-                    <button type="button" className="btn btn-link p-0" onClick={() => handleShowModal(index)}>
-                      <BiSolidEdit size="25" />
-                    </button>
+                    <div className="d-flex justify-content-start">
+                      <button type="button" className="btn btn-link p-0" onClick={() => handleShowModal(index)}>
+                        <BiSolidEdit size="25" />
+                      </button>
+                      {index >= 4 && (
+                        <button type="button" className="btn btn-link p-0" onClick={() => handleDeleteQualification(index)}>
+                          <TiDelete size="25" color='red' />
+                        </button>
+                      )}
+                    </div>
                   </td>
                 </tr>
               ))}
@@ -71,7 +172,7 @@ const ResearchQualifications = () => {
         </div>
         <div className="d-flex justify-content-end">
           <button type="submit" className="btn btn-success m-1 card-button">Save & Next</button>
-          <button type="submit" className="btn btn-light text-light  m-1 card-button" style={{backgroundColor:"#005174"}}>Skip This Step</button>
+          <button type="button" className="btn text-light m-1 card-button" style={{ backgroundColor: "#005174" }}>Skip This Step</button>
         </div>
       </div>
       {selectedRow !== null && (
@@ -83,57 +184,104 @@ const ResearchQualifications = () => {
             <form>
               <div className="mb-3">
                 <label className="form-label">Qualification Name</label>
-                <input type="text" className="form-control" value={qualifications[selectedRow]} readOnly />
+                <input type="text" className="form-control" value={qualifications[selectedRow].degree} readOnly />
               </div>
               <div className="mb-3">
                 <label className="form-label">University / Institution Name</label>
-                <input type="text" className="form-control" />
+                <input type="text" className="form-control" name="university" value={qualifications[selectedRow].university} onChange={handleQualificationChange} />
               </div>
               <div className="mb-3">
                 <label className="form-label">Subject / Title of Thesis</label>
-                <input type="text" className="form-control" />
+                <input type="text" className="form-control" name="subjects" value={qualifications[selectedRow].subjects} onChange={handleQualificationChange} />
+              </div>
+              <div className="mb-3">
+                <label className="form-label">Year</label>
+                <input type="text" className="form-control" name="year" value={qualifications[selectedRow].year} onChange={handleQualificationChange} />
               </div>
               <div className="mb-3">
                 <label className="form-label">Marks Type</label>
-                <select className="form-select">
+                <select className="form-select" name="marksType" value={qualifications[selectedRow].marksType} onChange={handleQualificationChange}>
                   <option value="percentage">Percentage</option>
                   <option value="cgpa">CGPA</option>
                 </select>
               </div>
               <div className="mb-3">
                 <label className="form-label">Percentage / CGPA</label>
-                <input type="text" className="form-control" />
-              </div>
-              <div className="mb-3">
-                <label className="form-label">Minimum Marks</label>
-                <input type="text" className="form-control" />
-              </div>
-              
-              <div className="mb-3">
-                <label className="form-label">Percentage / CGPA</label>
-                <input type="text" className="form-control" />
+                <input type="text" className="form-control" name="percentage" value={qualifications[selectedRow].percentage} onChange={handleQualificationChange} />
               </div>
               <div className="mb-3">
                 <label className="form-label">Degree Status</label>
-                <select className="form-select">
-                  <option value="first">Awarded</option>
-                  <option value="second">Submitted</option>
+                <select className="form-select" name="status" value={qualifications[selectedRow].status} onChange={handleQualificationChange}>
+                  <option value="Awarded">Awarded</option>
+                  <option value="Submitted">Submitted</option>
                 </select>
               </div>
               <div className="mb-3">
-                  <label className="form-label">Awarded / Submitted Date (dd/mm/yy)</label>
-                  <input type="date" className="form-control" />
-                </div>
+                <label className="form-label">Awarded / Submitted Date (dd/mm/yy)</label>
+                <input type="date" className="form-control" name="date" onChange={handleQualificationChange} />
+              </div>
             </form>
           </Modal.Body>
           <Modal.Footer>
             <Button variant="danger" onClick={handleCloseModal}>Close</Button>
-            <Button variant="primary">Save changes</Button>
+            <Button variant="primary" onClick={handleSaveChanges}>Save changes</Button>
           </Modal.Footer>
         </Modal>
       )}
+      <Modal show={showAddModal} onHide={handleCloseAddModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add Qualification</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form>
+            <div className="mb-3">
+              <label className="form-label">Degree</label>
+              <input type="text" className="form-control" name="degree" value={newQualification.degree} onChange={handleNewQualificationChange} />
+            </div>
+            <div className="mb-3">
+              <label className="form-label">University / Institution Name</label>
+              <input type="text" className="form-control" name="university" value={newQualification.university} onChange={handleNewQualificationChange} />
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Subject / Title of Thesis</label>
+              <input type="text" className="form-control" name="subjects" value={newQualification.subjects} onChange={handleNewQualificationChange} />
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Year</label>
+              <input type="text" className="form-control" name="year" value={newQualification.year} onChange={handleNewQualificationChange} />
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Marks Type</label>
+              <select className="form-select" name="marksType" value={newQualification.marksType} onChange={handleNewQualificationChange}>
+                <option value="percentage">Percentage</option>
+                <option value="cgpa">CGPA</option>
+              </select>
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Percentage / CGPA</label>
+              <input type="text" className="form-control" name="percentage" value={newQualification.percentage} onChange={handleNewQualificationChange} />
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Degree Status</label>
+              <select className="form-select" name="status" value={newQualification.status} onChange={handleNewQualificationChange}>
+                <option value="Awarded">Awarded</option>
+                <option value="Submitted">Submitted</option>
+              </select>
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Awarded / Submitted Date (dd/mm/yy)</label>
+              <input type="date" className="form-control" name="date" onChange={handleNewQualificationChange} />
+            </div>
+          </form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseAddModal}>Close</Button>
+          <Button variant="primary" onClick={handleAddQualification}>Add Qualification</Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
 
 export default ResearchQualifications;
+
